99b04d7658d1b14b0ebeb6e7c4112d7a39562d64
abigail pauls <apauls@haverford.edu>
2026-02-03 12:49:47 -0500

    got minimum and maximum dates into one dataframe

--- stations.R
+++ stations.R
@@ -13,13 +13,37 @@ clim.stations.1<-clim.stations[! clim.stations %in% c("NA.csv","US1COLR0556.csv"
 
 files<-file.path("NOAA_climate_data_1",clim.stations.1)
 
-data_list<-setNames(lapply(files,fread),tools::file_path_sans_ext(basename(files)))
+files.1<-files[file.exists(files)]
 
-precip<-list()
+data_list<-setNames(lapply(files.1,fread),tools::file_path_sans_ext(basename(files.1)))
 
-for(h in seq_along(data_list)){
-	if((is.na(data_list[[h]][[PRCP]])/nrows(data_list[[h]][[PRCP]]))<=0.1){
-	precip[h]<-data_list[h]}}
+clim<-rbindlist(lapply(files.1,function(f){
+dt<-fread(f)	
+data.table(
+	station_id = basename(f),
+	n_rows = nrow(dt),
+        min.date=min(dt$DATE, na.rm = TRUE),
+        max.date=max(dt$DATE, na.rm = TRUE))}))
+
+
+
+#clim <- vector("list", length(files.1))
+
+#for(i in seq_along(data_list)){
+#	clim[[i]]<-data.table(
+#	n_rows = nrow(data_list),
+#	station_id=basename(files.1),
+#	min.date=min(data_list[[i]]$DATE),
+#	max.date=max(data_list[[i]]$DATE))
+#}
+
+#clim.1<-rbindlist(clim)
+
+#precip<-list()
+
+#for(h in seq_along(data_list)){
+#	if(((is.na(data_list[[h]][[PRCP]])/(nrows(data_list[[h]][[PRCP]]))<=0.1){
+#	precip[h]<-data_list[h]}}
 
 #temp <- temp %>% 
 #  dplyr::mutate(min.year = NA)
@@ -29,17 +53,3 @@ for(h in seq_along(data_list)){
 #for(j in seq_along(stations)){
 #test$min.year[j]<-paste(min(j$year))
 #temp$max.year[j]<-paste(max(j$year))}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
