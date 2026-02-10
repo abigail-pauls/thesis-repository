45ead37b8ff1a506290e95cb29ab32f636b2cf50
abigail pauls <apauls@haverford.edu>
2026-02-10 16:56:59 -0500

    adding in temp metrics

--- temp.R
+++ temp.R
@@ -32,18 +32,37 @@
 
 #sites.1<-with(sites,subset(sites,jan>="670"&feb>="605"&mar>="670"&apr>="648"&may>="670"&jun>="648"&jul>="670"&aug>="670"&sep>="648"&oct>="670"&nov>="648"&dec>="670"))
 
-sites.1 <- sites.1 %>%
-  mutate(year = as.integer(year))
+#sites.1 <- sites.1 %>%
+#  mutate(year = as.integer(year))
 
-years<-1973:2023
+#years<-1973:2023
 
-sites.2 <- sites.1 %>% 
-	filter(year %in% years) %>%
-	distinct(station_id,year) %>%
-	group_by(station_id) %>%
-	filter(n_distinct(year) == length(years)) %>%
-	ungroup()
+#sites.2 <- sites.1 %>% 
+#	filter(year %in% years) %>%
+#	distinct(station_id,year) %>%
+#	group_by(station_id) %>%
+#	filter(n_distinct(year) == length(years)) %>%
+#	ungroup()
 
+soil<-read.csv("/disks/home/abigail/thesis-repository/lp.clim.csv")
+
+soil.temp<-merge(soil,temp.max.3,all.x=FALSE,all.y=TRUE)
+
+soil.temp$date<-as.Date(soil.temp$date)
+
+soil.temp<-soil.temp %>% mutate(start.date = date %m-% years(1))
+
+for(i in 1:nrow(soil.temp)){
+x<-fread(file.path("NOAA_climate_data_1",paste0(soil.temp$station_id[[i]],".csv")))
+x$DATE<-as.Date(x$DATE)
+x$TMAX<-as.numeric(x$TMAX)
+        time<-x$DATE<=soil.temp$date[i]&x$DATE>=soil.temp$start.date[i]
+        soil.temp$avg.temp.max[i]<-with(subset(x,x$DATE<=soil.temp$date[i]&x$DATE>=soil.temp$start.date[i]),mean(TMAX,na.rm=TRUE))
+	soil.temp$ex.high.temp.max[i]<-nrow(subset(x,time&x$TMAX>=soil.temp$ex.high.max[i]))
+	soil.temp$high.temp.max[i]<-nrow(subset(x,time&x$TMAX>=soil.temp$high.max[i]))	
+	soil.temp$ex.low.temp.max[i]<-nrow(subset(x,time&x$TMAX<=soil.temp$ex.low.max[i]))
+	soil.temp$low.temp.max[i]<-nrow(subset(x,time&x$TMAX<=soil.temp$low.max[i]))
+}
 
 #files<-file.path("NOAA_climate_data_1",clim.stations.1)
 
